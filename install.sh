6d3b070c3162c41de9d7b5d6b4d75d1a5c2511d2
mar <josecastanon4@gmail.com>
2024-12-10 10:40:15 -0800

    auto creating extralibs folder

--- install.sh
+++ install.sh
@@ -28,3 +28,5 @@ mv protonate $INSTALL_PATH/soft/DOCK-latest/ligand
 mv rdkit $INSTALL_PATH/soft/DOCK-latest/ligand
 mv reactor $INSTALL_PATH/soft/DOCK-latest/ligand
 mv strain $INSTALL_PATH/soft/DOCK-latest/ligand
+
+mkdir -p $INSTALL_PATH/soft/extralibs
