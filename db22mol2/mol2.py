14ca5d0dea001cfeda5c999c5e96b8c4f9523121
Brendan Hall <brendan.hall@ucsf.edu>
2026-02-24 11:23:32 -0800

    Fixes to db2tomol2

--- db22mol2/mol2.py
+++ db22mol2/mol2.py
@@ -1,4 +1,4 @@
-#!/user/bin/python
+#!/usr/bin/env python
 
 #################################################################################################################
 ##
@@ -10,10 +10,28 @@
 #################################################################################################################
 
 
-import math, sys
-import os.path
+import contextlib
+import gzip
+import math
+import os
 import cmath
-from math import sqrt
+
+@contextlib.contextmanager
+def null_context(value):
+    yield value
+
+
+def maybe_open(source, mode='r', context=False):
+    if isinstance(source, str):
+        if os.path.splitext(source)[1] == '.gz':
+            return gzip.open(source, mode)
+        else:
+            return open(source, mode)
+    elif context:
+        return null_context(source)
+    else:
+        return source
+
 
 #################################################################################################################
 #################################################################################################################
@@ -24,7 +42,7 @@ class Mol:
         self.name         = str(name)
         self.atom_list    = atom_list
         self.bond_list    = bond_list
-	self.residue_list = residue_list
+        self.residue_list = residue_list
 
 class atom:
     def __init__(self,X,Y,Z,Q,type,name,num,resnum,resname):
@@ -36,8 +54,8 @@ class atom:
         self.type = type
         self.name = name
         self.num  = int(num)
-	self.resnum  = int(resnum)
-	self.resname = resname
+        self.resnum  = int(resnum)
+        self.resname = resname
 class bond:
      def __init__(self,a1_num,a2_num,num,type):
         self.a1_num = int(a1_num)
@@ -46,8 +64,8 @@ class bond:
         self.type = type
 class residue:
      def __init__(self,atom_list,resnum,resname):
-	self.atom_list = atom_list
-	self.resnum  = int(resnum)
+        self.atom_list = atom_list
+        self.resnum  = int(resnum)
         self.resname = resname
 
 
@@ -82,9 +100,6 @@ def read_Mol2_lines(lines,startline):
     for lnum in range(startline,len(lines)):
          line = lines[lnum]
          linesplit = line.split() #split on white space
-         if line[0] == "#":
-            #print line
-            continue
          if (len(linesplit) == 1):
             if(linesplit[0] == "@<TRIPOS>MOLECULE"):
                flag_mol_set = True
@@ -140,13 +155,12 @@ def read_Mol2_lines(lines,startline):
              Q         = linesplit[8]
              temp_atom = atom(X,Y,Z,Q,atom_type,atom_name,atom_num,res_num,res_name)
              atom_list.append(temp_atom)
-	     if residue_list.has_key(res_num):
-     		 residue_list[res_num].append(temp_atom)
-    	     else:
-                 residue_list[res_num] = [temp_atom]
+             if res_num in residue_list:
+                residue_list[res_num].append(temp_atom)
+             else:
+                residue_list[res_num] = [temp_atom]
 
          elif (len(linesplit) == 4 and flag_bond):
-             #print line
              bond_num  = linesplit[0]
              a1_num    = linesplit[1]
              a2_num    = linesplit[2]
@@ -163,23 +177,23 @@ def read_Mol2_lines(lines,startline):
                  flag_nextmol = False
                  #atom_list = [];bond_list = []
                  #if (lnum != startline):
-                 print (lnum, startline )
+                 print(lnum, startline) 
                  break
  
     ## we are reading in one molecule at a time
     ID_heavy_atoms(atom_list)
     data = Mol('',Name,atom_list,bond_list,residue_list)
     atom_list = [];bond_list = []
-    print ("flag_mol_set", flag_mol_set )
+    print("flag_mol_set", flag_mol_set) 
 
     return flag_mol_set, data, lnum
 
 #################################################################################################################
 #################################################################################################################
-def read_Mol2_file(file):
+def read_Mol2_file(source):
     # reads in data from multi-Mol2 file.
 
-    file1 = open(file,'r')
+    file1 = maybe_open(source,'r')
     lines  =  file1.readlines()
     file1.close()
 
@@ -198,9 +212,6 @@ def read_Mol2_file(file):
     i = 0  # i is the num of molecules read so far
     for line in lines:
          linesplit = line.split() #split on white space
-         if line[0] == "#":
-            #print line
-            continue
          if (len(linesplit) == 1):
             if(linesplit[0] == "@<TRIPOS>MOLECULE"):
                if flag_frist_mol:
@@ -261,10 +272,10 @@ def read_Mol2_file(file):
              #print X,Y,Z,Q,atom_type,atom_name,atom_num,res_num,res_name
              temp_atom = atom(X,Y,Z,Q,atom_type,atom_name,atom_num,res_num,res_name)
              atom_list.append(temp_atom)
-	     if residue_list.has_key(res_num):
-     		 residue_list[res_num].append(temp_atom)
-    	     else:
-                 residue_list[res_num] = [temp_atom]
+             if res_num in residue_list:
+                residue_list[res_num].append(temp_atom)
+             else:
+                residue_list[res_num] = [temp_atom]
 
          elif (len(linesplit) == 4 and flag_bond):
              bond_num  = linesplit[0]
@@ -290,10 +301,10 @@ def read_Mol2_file(file):
     return mol_list
 #################################################################################################################
 #################################################################################################################
-def read_Mol2_file_head(file):
+def read_Mol2_file_head(source):
     # reads in data from multi-Mol2 file.
 
-    file1 = open(file,'r')
+    file1 = maybe_open(source,'r')
     lines  =  file1.readlines()
     file1.close()
 
@@ -384,10 +395,10 @@ def read_Mol2_file_head(file):
              #print X,Y,Z,Q,atom_type,atom_name,atom_num,res_num,res_name
              temp_atom = atom(X,Y,Z,Q,atom_type,atom_name,atom_num,res_num,res_name)
              atom_list.append(temp_atom)
-	     if residue_list.has_key(res_num):
-     		 residue_list[res_num].append(temp_atom)
-    	     else:
-                 residue_list[res_num] = [temp_atom]
+             if res_num in residue_list:
+                residue_list[res_num].append(temp_atom)
+             else:
+                residue_list[res_num] = [temp_atom]
 
          elif (len(linesplit) == 4 and flag_bond):
              bond_num  = linesplit[0]
@@ -414,111 +425,74 @@ def read_Mol2_file_head(file):
     return mol_list
 #################################################################################################################
 #################################################################################################################
-def write_mol2(molecule,filename):
+def write_mol2(molecule,filename, mode='w'):
 
         # define a dictionary for help renumbering
         atom_dic = {}
         resid_dic = {}
-        count = 1
-        for atom in molecule.atom_list:
-            if not atom_dic.has_key(atom.num):
-               atom_dic[atom.num] = count
-               #print atom.num, ",", count,",", atom_dic[atom.num]
-               count=count+1
-        count = 1
-        for resnum in molecule.residue_list.keys():
-            resid_dic[resnum] = count
-            count=count+1
+        for idx, atom in enumerate(molecule.atom_list, start=1):
+            atom_dic.setdefault(atom.num, idx)
 
-	outmol2 = open(filename,'w')
-        #print molecule.header
-	outmol2.write(molecule.header)      #dock info after #s 
-	outmol2.write("@<TRIPOS>MOLECULE\n")      #start the MOLECULE RTI (Record Type Indicator)
-	outmol2.write(molecule.name+'\n')         #print MOL2FILE name of the molecule
-	outmol2.write("%-5d %-5d %-5d 0     0\n" % (len(molecule.atom_list), 
-		len(molecule.bond_list), len(molecule.residue_list.keys()))) 
-	# For now, the number of residues is hard-coded to 1. To be fixed.
-	outmol2.write("SMALL\n") 		  #mol_type
-	outmol2.write("USER_CHARGES\n") 	  #charge_type
-
-	outmol2.write("@<TRIPOS>ATOM\n")      #start the ATOM RTI (Record Type Indicator)
-	for j in range(0,len(molecule.atom_list)):
-                #print atom_dic[molecule.atom_list[j].num], molecule.atom_list[j].num
-        	outmol2.write("%-6d %-4s %9.4f %9.4f %9.4f %-5s %4s %6s %9.4f\n" % 
-		(atom_dic[molecule.atom_list[j].num], molecule.atom_list[j].name, molecule.atom_list[j].X, molecule.atom_list[j].Y, 
-		molecule.atom_list[j].Z, molecule.atom_list[j].type, resid_dic[molecule.atom_list[j].resnum], 
-		molecule.atom_list[j].resname, molecule.atom_list[j].Q))
-
-	outmol2.write("@<TRIPOS>BOND\n")
         count = 1
-	for m in range(0,len(molecule.bond_list)):
-        	outmol2.write("%-5d %-5d %-5d %s\n" % (count, 
-		atom_dic[molecule.bond_list[m].a1_num], atom_dic[molecule.bond_list[m].a2_num], molecule.bond_list[m].type))
-                count = count + 1
+        for idx, resnum in enumerate(molecule.residue_list.keys(), start=1):
+            resid_dic.setdefault(resnum, count)
 
-	outmol2.write("@<TRIPOS>SUBSTRUCTURE\n")
-        count = 1
-	for resnum in molecule.residue_list.keys():
-		#outmol2.write("%-3d %-5s %-5d RESIDUE    1   A     %-5s 1\n" % (resnum, 
-		outmol2.write("%-3d %-5s %-5d RESIDUE    1   A     %-5s 1\n" % (resid_dic[resnum], 
-		molecule.residue_list[resnum][0].resname, # residue name 
-		atom_dic[molecule.residue_list[resnum][0].num], molecule.residue_list[resnum][0].resname[0:3]))   # atom num of first atom in this residue
-                count = count + 1
-	outmol2.close()
-    	return
-#################################################################################################################
-def append_mol2(molecule,filename):
+        outmol2 = maybe_open(filename,mode)
 
-        # define a dictionary for help renumbering
-        atom_dic = {}
-        resid_dic = {}
-        count = 1
-        for atom in molecule.atom_list:
-            if not atom_dic.has_key(atom.num):
-               atom_dic[atom.num] = count
-               #print atom.num, ",", count,",", atom_dic[atom.num]
-               count=count+1
-        count = 1
-        for resnum in molecule.residue_list.keys():
-            resid_dic[resnum] = count
-            count=count+1
-
-        outmol2 = open(filename,'a')
-        #print molecule.header
-	outmol2.write(molecule.header)      #dock info after #s 
+        outmol2.write(molecule.header)      #dock info after #s
         outmol2.write("@<TRIPOS>MOLECULE\n")      #start the MOLECULE RTI (Record Type Indicator)
         outmol2.write(molecule.name+'\n')         #print MOL2FILE name of the molecule
-        outmol2.write("%-5d %-5d %-5d 0     0\n" % (len(molecule.atom_list),
-                len(molecule.bond_list), len(molecule.residue_list.keys())))
+        outmol2.write("%-5d %-5d %-5d 0     0\n" % (
+            len(molecule.atom_list), 
+            len(molecule.bond_list), 
+            len(molecule.residue_list.keys())
+            )
+        )
         # For now, the number of residues is hard-coded to 1. To be fixed.
         outmol2.write("SMALL\n")                  #mol_type
         outmol2.write("USER_CHARGES\n")           #charge_type
 
         outmol2.write("@<TRIPOS>ATOM\n")      #start the ATOM RTI (Record Type Indicator)
-        for j in range(0,len(molecule.atom_list)):
-                #print atom_dic[molecule.atom_list[j].num], molecule.atom_list[j].num
-                outmol2.write("%-6d %-4s %9.4f %9.4f %9.4f %-5s %4s %6s %9.4f\n" %
-                (atom_dic[molecule.atom_list[j].num], molecule.atom_list[j].name, molecule.atom_list[j].X, molecule.atom_list[j].Y,
-                molecule.atom_list[j].Z, molecule.atom_list[j].type, resid_dic[molecule.atom_list[j].resnum],
-                molecule.atom_list[j].resname, molecule.atom_list[j].Q))
+        for j, atom in enumerate(molecule.atom_list):
+                    #print atom_dic[molecule.atom_list[j].num], molecule.atom_list[j].num
+                outmol2.write("%-6d %-4s %9.4f %9.4f %9.4f %-5s %4s %6s %9.4f\n" % (
+                        atom_dic[atom.num], 
+                        atom.name, 
+                        atom.X, 
+                        atom.Y, 
+                        atom.Z, 
+                        atom.type, 
+                        resid_dic[atom.resnum], 
+                        atom.resname, 
+                        atom.Q
+                    )
+                )
 
         outmol2.write("@<TRIPOS>BOND\n")
-        count = 1
-        for m in range(0,len(molecule.bond_list)):
-                outmol2.write("%-5d %-5d %-5d %s\n" % (count,
-                atom_dic[molecule.bond_list[m].a1_num], atom_dic[molecule.bond_list[m].a2_num], molecule.bond_list[m].type))
-                count = count + 1
+        for count, bond in enumerate(molecule.bond_list, start=1):
+                outmol2.write("%-5d %-5d %-5d %s\n" % (
+                        count,
+                        atom_dic[bond.a1_num],
+                        atom_dic[bond.a2_num],
+                        bond.type
+                    )
+                )
 
         outmol2.write("@<TRIPOS>SUBSTRUCTURE\n")
-        count = 1
         for resnum in molecule.residue_list.keys():
-                #outmol2.write("%-3d %-5s %-5d RESIDUE    1   A     %-5s 1\n" % (resnum, 
-                outmol2.write("%-3d %-5s %-5d RESIDUE    1   A     %-5s 1\n" % (resid_dic[resnum],
-                molecule.residue_list[resnum][0].resname, # residue name 
-                atom_dic[molecule.residue_list[resnum][0].num], molecule.residue_list[resnum][0].resname[0:3]))   # atom num of first atom in this residue
-                count = count + 1
-        outmol2.close()
-        return
+                #outmol2.write("%-3d %-5s %-5d RESIDUE    1   A     %-5s 1\n" % (resnum,
+                 outmol2.write("%-3d %-5s %-5d RESIDUE    1   A     %-5s 1\n" % (
+                    resid_dic[resnum],
+                    molecule.residue_list[resnum][0].resname, # residue name
+                    atom_dic[molecule.residue_list[resnum][0].num],
+                    molecule.residue_list[resnum][0].resname[0:3]
+                )
+            )   # atom num of first atom in this residue
+
+
+#################################################################################################################
+def append_mol2(molecule,filename, mode='a'):
+    return write_mol2(molecule, filename, mode)
 
 #################################################################################################################
 # this fuction will convert the sybyl atomtypes to the DOCK3.7 atomtype.
@@ -581,8 +555,7 @@ def convert_sybyl_to_dock (molecule):
                          'Mo': 25,
                          'Mn': 25,
                          'Co.oh': 25}
-  #i = 0
-  i = 1 # atoms labels in bonds start at one.
+  i = 0
   docktype = []
   # loop over all atoms in molecule
   for atom in molecule.atom_list:
@@ -591,27 +564,22 @@ def convert_sybyl_to_dock (molecule):
          # if atom type is a hydrogen, loop over the bonds, see if it is attached to a carbon
          hflag = False
          for bond in molecule.bond_list:
-             #print "looking at bond ", bond.num, " (", bond.a1_num, bond.a2_num,")"
              if (i == bond.a1_num):
-                 #print i, "->", bond.a2_num
-                 #print "bond (", molecule.atom_list[bond.a1_num-1].type, molecule.atom_list[bond.a2_num-1].type,")"
                  j = bond.a2_num
                  hflag = True
                  break # hydrogens are only attached to on atom. 
              elif (i == bond.a2_num):
-                 #print i, "<-", bond.a1_num
-                 #print "bond (", molecule.atom_list[bond.a1_num-1].type, molecule.atom_list[bond.a2_num-1].type,")"
                  j = bond.a1_num
                  hflag = True
                  break # hydrogens are only attached to on atom. 
          if hflag:
-            if molecule.atom_list[j-1].type in [ 'C.1', 'C.3', 'C.2', 'C.ar', 'C.cat']:
-               print (i, atom.type, j, molecule.atom_list[j-1].type)
+            if molecule.atom_list[j].type in [ 'C.1','C.3', 'C.2', 'C.ar']:
+               print(i, atom.type, molecule.atom_list[j].type)
                type = 'H-C'
             #else: 
             #   type = atom.type
-         else: 
-            print ("ERROR.")
+         else:
+            print("ERROR.")
             exit()
       #else:
       #   type = atom.type
@@ -636,7 +604,7 @@ def ID_heavy_atoms(atom_list):
 #################################################################################################################
 def distance2_vec(vector1,vector2):
         if (len(vector1)!=len(vector2)):
-                print ('function distance(): vectors differ in length')
+                print('function distance(): vectors differ in length')
                 sys.exit(1)
         distance2 = 0
         for i in range(len(vector1)):
@@ -666,7 +634,7 @@ def heavy_atom_RMSD(ref,pose):
         if (ref.atom_list[i].heavy_atom and pose.atom_list[i].heavy_atom):
            sum += distance2(ref.atom_list[i],pose.atom_list[i])
            num_hvy_atoms+=1
-    return sqrt(sum/num_hvy_atoms)
+    return math.sqrt(sum/num_hvy_atoms)
 
 #################################################################################################################
 #################################################################################################################
@@ -734,7 +702,7 @@ def calc_dipole_moment(molecule):
         uJsum += molecule.atom_list[k].Q * (molecule.atom_list[k].Y - cmass[1])
         uKsum += molecule.atom_list[k].Q * (molecule.atom_list[k].Z - cmass[2])
 
-    umag          = sqrt( (uIsum*uIsum) + (uJsum*uJsum) + (uKsum*uKsum) )
+    umag          = math.sqrt( (uIsum*uIsum) + (uJsum*uJsum) + (uKsum*uKsum) )
     dipolemoment  = umag*conversion;
     uvector = [uIsum,uJsum,uKsum]
 
@@ -759,8 +727,8 @@ def remove_hydrogens(m):
         retain_bond = True
         for atom_id in range(len(m.atom_list)):
            if (m.atom_list[atom_id].heavy_atom):
-              continue  
-	   # Atoms down here are always hydrogen 
+              continue
+           # Atoms down here are always hydrogen
            if (m.bond_list[bond_id].a1_num == m.atom_list[atom_id].num):
               retain_bond = False 
            if (m.bond_list[bond_id].a2_num == m.atom_list[atom_id].num):
